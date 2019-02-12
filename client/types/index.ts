40b30b3240982a4c923116717ae3043a88ddcdc8
nikolasmelui <slak@samaradom.ru>
2019-02-12 23:58:03 +0400

    Change entity id fields type from number to string

--- client/types/index.ts
+++ client/types/index.ts
@@ -1,20 +1,22 @@
 export * from './state';
 
 export interface Page {
-  id: number;
+  _id: string;
   _slug: string;
   title: string;
   description: string;
 }
 
 export interface Post {
-  id: number;
+  _id: string;
+  _slug: string;
   title: string;
   description: string;
 }
 
 export interface Project {
-  id: number;
+  _id: string;
+  _slug: string;
   title: string;
   description: string;
 }
